0f0f2dde360cf44d97d20bdeb8fa8a1f6b99c2b9
Grish Narayanan S <grishnarayanan.prof@gmail.com>
2026-01-19 21:03:33 +0530

    Update firebase.js

--- OneDrive/Documents/frontends/vantage/src/lib/firebase.js
+++ OneDrive/Documents/frontends/vantage/src/lib/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCXZ4Pj__2NOHV9e99Ds0-nBLv8jfQrcIA",
-  authDomain: "vantage-v1.firebaseapp.com",
-  projectId: "vantage-v1",
-  storageBucket: "vantage-v1.firebasestorage.app",
-  messagingSenderId: "1078074596514",
-  appId: "1:1078074596514:web:d9860c62a530a9e2a04bfb"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
